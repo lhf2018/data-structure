b50c13918455a3f0b2cc1e27a132108fadf2ba0b
1146953160 <1146953160@qq.com>
2019-08-04 11:27:37 +0800

    更新了：斐波那契数列

---
+++ src/Recursion/Fibonacci.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Fibonacci {
+    public int getF(int n){
+        if(n<0){
+            return -1;
+        }else if(n==0){
+            return 0;
+        }else if(n==1||n==2){
+            return 1;
+        }else{
+            return getF(n-1)+getF(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci=new Fibonacci();
+        for(int i=0;i<20;i++){
+            System.out.print(fibonacci.getF(i)+" ");
+        }
+    }
+}
