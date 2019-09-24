3d565e0cd0f489b55859361dc8813907070f5ea5
lhf2018 <1146953160@qq.com>
2019-09-24 12:28:16 +0800

    队列

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 * KMP算法 KMP
 #### 栈和队列 StackandQueue
 * 栈 Stack
+* 队列 Queue
 #### 查找算法 find
 * 顺序查找 OrderSearch
 * 二分查找 BinSearch
---
+++ src/StackandQueue/Queue.java
@@ -0,0 +1,93 @@
+package StackandQueue;
+
+/**
+ * 本例用链表实现了一个队列，并实现了push、peek、pop基本操作
+ */
+public class Queue {
+    /**
+     * 节点值
+     */
+    class Node{
+        private int data;
+        private Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+    }
+    private Node frontNode; //首节点
+    private Node rearNode;  //尾节点
+    private int size;
+    public Queue(){
+        this.frontNode=null;
+        this.rearNode=null;
+        this.size=0;
+    }
+
+    /**
+     * 判断队列是否为空
+     */
+    public boolean isEmpty(){
+        return frontNode==null;
+    }
+    public void push(int data){
+        Node newnode=new Node(data);
+        //位指针不为空，设置下一位
+        if(rearNode!=null){
+            rearNode.next=newnode;
+        }
+        rearNode=newnode;
+        //首为空，意味着刚刚设置了一个尾节点
+        if(frontNode==null){
+            frontNode=rearNode;
+        }
+        size++;
+    }
+
+    /**
+     * 弹出队列
+     */
+    public int pop() throws Exception {
+       int data=0;
+       if(frontNode!=null){
+           data=frontNode.getData();
+           frontNode=frontNode.next;
+           size--;
+       }else {
+           throw new Exception("Queue Empty");
+       }
+       return data;
+    }
+
+    /**
+     * 查看首端的值
+     */
+    public int peek() throws Exception {
+        if(frontNode!=null){
+            return frontNode.getData();
+        }else {
+            throw new Exception("Queue Empty");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Queue queue=new Queue();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        int len=queue.size;
+        for(int i=0;i<len;i++){
+            System.out.println(queue.pop());
+        }
+    }
+
+}
